48758228829de93a7a9d31bb3f33f51b2e0824ba
sjonpaulbrown <sjonpaulbrown@gmail.com>
2022-03-17 14:08:55 -0400

    Safely reference script value

--- cmd/util/ledger/reporters/export_reporter.go
+++ cmd/util/ledger/reporters/export_reporter.go
@@ -42,25 +42,26 @@ func (e *ExportReporter) Report(payload []ledger.Payload, o ledger.ExportOutputs
 			Info().
 			Uint64("epochCounter", epochCounter).
 			Msg("Fetched epoch counter from the FlowEpoch smart contract")
+
+			report := ExportReport{
+				CurrentStateCommitment:  o.CurrentStateCommitement,
+				EpochCounter:            script.Value.ToGoValue().(uint64),
+				PreviousStateCommitment: e.PreviousStateCommitment,
+			}
+			file, _ := json.MarshalIndent(report, "", " ")
+			e.Log.
+				Info().
+				Str("ExportReport", string(file)).
+				Msg("Ledger Export has finished")
+			_ = ioutil.WriteFile("export_report.json", file, 0644)
+			return nil
 	} else {
 		e.Log.
 			Error().
 			Err(script.Err).
 			Msg("Failed to get epoch counter")
+		return nil
 	}
-
-	report := ExportReport{
-		CurrentStateCommitment:  o.CurrentStateCommitement,
-		EpochCounter:            script.Value.ToGoValue().(uint64),
-		PreviousStateCommitment: e.PreviousStateCommitment,
-	}
-	file, _ := json.MarshalIndent(report, "", " ")
-	e.Log.
-		Info().
-		Str("ExportReport", string(file)).
-		Msg("Ledger Export has finished")
-	_ = ioutil.WriteFile("export_report.json", file, 0644)
-	return nil
 }
 
 var _ ledger.Reporter = &ExportReporter{}
